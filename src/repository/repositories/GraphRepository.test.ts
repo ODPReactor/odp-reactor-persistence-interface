dd3986a7b6a8857821003a0718209e10f24eb214
colin <colin@ubuntu.com>
2021-07-23 11:35:12 -0400

    correct failing test

--- src/repository/repositories/GraphRepository.test.ts
+++ src/repository/repositories/GraphRepository.test.ts
@@ -1,56 +1,66 @@
+import { SparqlGraphTestHelper } from '../clients/sparql/test/SparqlGraphTestHelper'
 import { GraphRepository} from './GraphRepository'
 
 describe('GraphRepository create and drops graphs', () => {
 
+    const graphTester = new SparqlGraphTestHelper()
     const testEndpoint = process.env.TEST_SPARQL_ENDPOINT_URI
     if (!testEndpoint) {
         throw new Error("Cannot execute test, no TEST_SPARQL_ENDPOINT_URI variable found in .env")
     }
+    graphTester.setTestSparqlEndpoint(testEndpoint)
+
     console.log("[!] (WARN) Be sure test sparql endpoint has update value to true")
     const graphRepo = new GraphRepository()
     graphRepo.setEndpoint(testEndpoint)
 
-    const testGraph = 'http://example.com/graph0'
 
     test('it should not have graph to be created', async ()=> {
-        await graphRepo.delete(testGraph)
-        const hasGraph = await graphRepo.exists(testGraph)
+        const graph  = graphTester.getUniqueGraphId()
+        await graphRepo.delete(graph)
+        const hasGraph = await graphRepo.exists(graph)
         expect(hasGraph).toBeFalsy()
-        await graphRepo.delete(testGraph)
+        await graphRepo.delete(graph)
     })
     test('it should create a new graph', async ()=> {
-        await graphRepo.delete(testGraph)
-        await graphRepo.create(testGraph)
-        await graphRepo.delete(testGraph)
+        const graph  = graphTester.getUniqueGraphId()
+        await graphRepo.delete(graph)
+        await graphRepo.create(graph)
+        await graphRepo.delete(graph)
     })
     test('it should insert triple in a graph', async () => {
-        await graphRepo.delete(testGraph)
-        await graphRepo.create(testGraph)
-        await graphRepo.insertTriple(testGraph, '<ex:s> <ex:p> <ex:o>')
-        const hasGraph = await graphRepo.exists(testGraph)
+        const graph  = graphTester.getUniqueGraphId()
+        await graphRepo.delete(graph)
+        await graphRepo.create(graph)
+        await graphRepo.insertTriple(graph, '<ex:s> <ex:p> <ex:o>')
+        const hasGraph = await graphRepo.exists(graph)
         expect(hasGraph).toBeTruthy()
-        await graphRepo.delete(testGraph)
+        await graphRepo.delete(graph)
     })
     test('it should drop an existing graph', async () => {
-        await graphRepo.delete(testGraph)
-        await graphRepo.create(testGraph)
-        await graphRepo.delete(testGraph)
+        const graph  = graphTester.getUniqueGraphId()
+        await graphRepo.delete(graph)
+        await graphRepo.create(graph)
+        await graphRepo.delete(graph)
     })
     test('it should silently drop a non existing graph', async () => {
-        await graphRepo.delete(testGraph)
-        await graphRepo.delete(testGraph)
+        const graph  = graphTester.getUniqueGraphId()
+        await graphRepo.delete(graph)
+        await graphRepo.delete(graph)
     })
     test('it should not have graph after deletion', async ()=> {
-        await graphRepo.delete(testGraph)
-        const hasGraph = await graphRepo.exists(testGraph)
+        const graph  = graphTester.getUniqueGraphId()
+        await graphRepo.delete(graph)
+        const hasGraph = await graphRepo.exists(graph)
         expect(hasGraph).toBeFalsy()
-        await graphRepo.delete(testGraph)
+        await graphRepo.delete(graph)
     })
     test('it should silently ignore creating two times the same graph', async ()=> {
-        await graphRepo.delete(testGraph)
-        await graphRepo.create(testGraph)
-        await graphRepo.create(testGraph)
-        await graphRepo.delete(testGraph)
+        const graph  = graphTester.getUniqueGraphId()
+        await graphRepo.delete(graph)
+        await graphRepo.create(graph)
+        await graphRepo.create(graph)
+        await graphRepo.delete(graph)
     })
 })
 
